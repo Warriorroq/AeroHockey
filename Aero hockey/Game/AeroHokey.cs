57a1c4d6aed31d9c3bfc6d6d2cf564a6d1558289
Warriorroq <Warriorroq@gmail.com>
2021-04-29 08:56:19 +0300

    Created class Scene and game class

---
+++ Aero hockey/Game/AeroHokey.cs
@@ -0,0 +1,43 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+namespace Aero_hockey.Game
+{
+    public class AeroHokey : GameLoop
+    {
+        Scene scene;
+        Ball ball;
+        public AeroHokey() : base("Aero hokey")
+        {
+            scene = new Scene();
+        }
+        public override void Init()
+        {
+            ball = new Ball(scene, new RectangleShape(new Vector2f(10, 10))
+            {
+                FillColor = Color.Black
+            });
+            scene.Init();
+            scene.Add(ball);
+        }
+        public override void LoadContent()
+        {
+            DebugUtility.LoadContent(Fonts.CEAZAR);
+        }
+        public override void Update()
+        {
+            scene.Update();
+            if(Keyboard.IsKeyPressed(Keyboard.Key.Space))
+            {
+                ball.direction = new Vector2f(0, 0);
+            }
+        }
+        public override void Draw()
+        {
+            scene.Draw();
+            DebugFPS();
+        }
+        private void DebugFPS()
+            => Debug($"FPS:{1 / Time.deltaTime:0.0}");
+    }
+}
---
+++ Aero hockey/Game/Scene.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Aero_hockey.Game
+{
+    public class Scene
+    {
+        public event Action update;
+        public event Action draw;
+        private List<GameObject> _objects;
+        private List<GameObject> _objectsForDestroy;
+        public void Init()
+        {
+            _objects = new List<GameObject>();
+            _objectsForDestroy = new List<GameObject>();
+        }
+        public void Update()
+        {
+            update?.Invoke();
+            DestroyObjects();
+        }
+        public void Draw()
+        {
+            draw?.Invoke();
+        }
+        public void Add(GameObject obj)
+        {
+            _objects.Add(obj);
+        }
+        public void Destroy(GameObject obj)
+        {
+            _objectsForDestroy.Add(obj);
+        }
+        private void DestroyObjects()
+        {
+            foreach(var obj in _objectsForDestroy)
+            {
+                _objects.Remove(obj);
+                obj.Destroy();
+            }
+        }
+    }
+}

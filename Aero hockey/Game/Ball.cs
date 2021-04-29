f60046ad9de8348bc5a58a84aa0e1f385478b5c0
Warriorroq <Warriorroq@gmail.com>
2021-04-29 08:56:32 +0300

    Created class GameObject and Ball

---
+++ Aero hockey/Game/Ball.cs
@@ -0,0 +1,27 @@
+﻿using SFML.Graphics;
+using SFML.System;
+namespace Aero_hockey.Game
+{
+    public class Ball : GameObject
+    {
+        public Vector2f direction;
+        public Ball(Scene scene, Shape shape) : base(scene, shape)
+        {
+            position = new Vector2f(200, 200);
+            direction = new Vector2f(200, 100);
+            this.shape.Origin = new Vector2f(10, 10) / 2f;
+        }
+        public override void OnUpdate()
+        {
+            position += direction * Time.deltaTime;
+            if (position.X > Screen.widthWindow || position.X < 0)
+                direction.X = -direction.X;
+            if (position.Y > Screen.heightWindow || position.Y < 0)
+                direction.Y = -direction.Y;
+        }
+        public override void OnDestroy()
+        {
+            
+        }
+    }
+}
---
+++ Aero hockey/Game/GameObject.cs
@@ -0,0 +1,51 @@
+﻿using SFML.System;
+using SFML.Graphics;
+using System;
+namespace Aero_hockey.Game
+{
+    public abstract class GameObject
+    {
+        public Vector2f position;
+        public Action Destroy;
+        protected Shape shape;
+        public GameObject(Scene scene, Shape drawable)
+        {
+            shape = drawable;
+            if(!(scene is null))
+            {
+                CreateSceneBind(scene);
+            }
+            else
+            {
+                Destroy = (() => OnDestroy());
+            }
+        }
+        private void CreateSceneBind(Scene scene)
+        {
+            scene.draw += Draw;
+            scene.update += Update;
+            Destroy = (() => {
+                OnDestroy();
+                scene.draw -= Draw;
+                scene.update -= Update;
+            });
+        }
+        public void Update()
+        {
+            shape.Position = position;
+            OnUpdate();
+        }
+        public virtual void OnDestroy()
+        {
+
+        }
+        public virtual void OnUpdate()
+        {
+
+        }
+        public virtual void Draw()
+        {
+            Screen.window.Draw(shape);
+        }
+    }
+}
